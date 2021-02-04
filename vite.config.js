0538dfa1a420a946c260affe6bb4d1b87dd068a6
唐海 <280503392@qq.com>
2021-02-04 09:40:34 +0800

    clear

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  root: "./example",
-  alias: {
-    "/@/": path.resolve(__dirname, "src"),
-    "/d/": path.resolve(__dirname, "dist"),
-  },
-};
